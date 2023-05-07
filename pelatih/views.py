140ab438bd88cd982b63d5d61994de00f2cf74f8
Jessica Lambok <112997959+jessicalambok14@users.noreply.github.com>
2023-05-07 20:31:55 +0700

    Update views.py

--- pelatih/views.py
+++ pelatih/views.py
@@ -7,3 +7,9 @@ def register_pelatih(request):
 def dashboard_pelatih(request):
     return render(request, "dashboard_pelatih.html")
 
+def daftar_atlet(request):
+    return render(request, "daftar_atlet.html")
+
+def list_atlet(request):
+    return render(request, "list_atlet.html")
+
