f4c9c4f06dc62fd15165d964807b32e607863fe6
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2023-05-07 14:32:24 +0700

    Create Register Page

--- general/views.py
+++ general/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, "general/login.html")
+
+def register(request):
+    return render(request, "general/register.html")
