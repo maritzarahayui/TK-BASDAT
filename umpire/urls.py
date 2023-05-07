76b225e32d605f17f2b90b144b84aaeaea1a5cfc
maritzarahayu <maritzarahayui@gmail.com>
2023-05-07 13:22:06 +0700

    add CRUD pertandingan

--- umpire/urls.py
+++ umpire/urls.py
@@ -4,5 +4,9 @@ from umpire import views
 app_name = "umpire"
 
 urlpatterns = [
-  
+  path('', views.pertandingan_page, name="pertandingan_page"),
+  path('semifinal/', views.semifinal_page, name="semifinal_page"),
+  path('juara-3/', views.juara3_page, name="juara3_page"),
+  path('final/', views.final_page, name="final_page"),
+  path('hasil-pertandingan/', views.hasil_pertandingan, name="hasil_pertandingan")
 ]
--- umpire/views.py
+++ umpire/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def pertandingan_page(request):
+    return render(request, "pertandingan.html")
+
+def semifinal_page(request):
+    return render(request, "semifinal_page.html")
+
+def juara3_page(request):
+    return render(request, "juara3_page.html")
+
+def final_page(request):
+    return render(request, "final_page.html")
+
+def hasil_pertandingan(request):
+    return render(request, "hasil_pertandingan.html")
