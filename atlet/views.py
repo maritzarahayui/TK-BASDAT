82997c1f7a5659131234201020dab5a62e7c8849
Rahma Adinda Putri <rahma.adinda@ui.ac.id>
2023-05-22 17:44:27 +0700

    backend sponsor

--- atlet/views.py
+++ atlet/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from collections import namedtuple
+from django.db import connection
+import psycopg2
 
+def connectDb():
+    return psycopg2.connect(
+        user='postgres',
+        password='VqhDHXgGPL8avdUHqCWP',
+        host='containers-us-west-72.railway.app',
+        databases='railway',
+        port='7729',
+    )
+    
+def namedtuplefetchall(cursor):
+    "Return all rows from a cursor as a namedtuple"
+    desc = cursor.description
+    nt_result = namedtuple('Result', [col[0] for col in desc])
+    return [nt_result(*row) for row in cursor.fetchall()]
+
+def get_query(str):
+    '''Execute SQL query and return its result as a list'''
+    cursor = connection.cursor()
+    result = []
+    try:
+        cursor.execute(str)
+        result = namedtuplefetchall(cursor)
+    except Exception as e:
+        result = e
+    finally:
+        cursor.close()
+        return result
 
 def register_atlet(request):
     return render(request, "register_atlet.html")
@@ -34,4 +64,58 @@ def enrolled_event(request):
 
 
 def daftar_sponsor(request):
-    return render(request, "daftar_sponsor.html")
+    # id = str(request.session["id"]).strip()
+    id = 'aa8a676a-07a3-4eb6-bcec-54a74ee35c93'
+
+    query = get_query(
+        f'''SELECT nama_brand
+        FROM sponsor
+        WHERE id NOT IN 
+        (SELECT id_sponsor FROM atlet_sponsor WHERE id_atlet = '{id}')
+        '''
+    )
+    
+    if request.method != "POST":
+        return render(request, "daftar_sponsor.html", {"query":query})
+    
+    nama_brand = request.POST["sponsor"]
+    tgl_mulai = request.POST["tgl_selesai"]
+    tgl_selesai = request.POST["tgl_selesai"]
+    
+    id_sponsor = get_query(
+        f'''SELECT id
+        FROM SPONSOR
+        WHERE nama_brand = '{nama_brand}'
+        '''
+    )
+    
+    print(id_sponsor[0].id)
+    
+    get_query(
+        f'''INSERT INTO atlet_sponsor
+        VALUES ('{id}', '{id_sponsor[0].id}', '{tgl_mulai}', '{tgl_selesai}');
+        '''
+    )
+    
+    query = get_query(
+        f'''SELECT nama_brand
+        FROM sponsor
+        WHERE id NOT IN 
+        (SELECT id_sponsor FROM atlet_sponsor WHERE id_atlet = '{id}')
+        '''
+    )
+
+    return render(request, "daftar_sponsor.html", {"query":query})
+
+def list_sponsor(request):
+    # id = str(request.session["id"]).strip()
+    id = 'aa8a676a-07a3-4eb6-bcec-54a74ee35c93'
+
+    query = get_query(
+        f'''SELECT nama_brand, tgl_mulai, tgl_selesai
+        FROM atlet_sponsor, sponsor
+        WHERE id_sponsor = id AND ID_atlet = '{id}'
+        '''
+    )
+    
+    return render(request, "list_sponsor.html", {"query" : query})
