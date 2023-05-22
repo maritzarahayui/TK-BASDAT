66ad80418269ec1c255ee18e7d9cb03698eb788f
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2023-05-22 17:34:28 +0700

    Sql Form Kualifikasi

--- atlet/views.py
+++ atlet/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 import psycopg2
+from django.db import *
 
-def connectDb():
-    return psycopg2.connect(
-        user='postgres',
-        password='VqhDHXgGPL8avdUHqCWP',
-        host='containers-us-west-72.railway.app',
-        databases='railway',
-        port='7729',
-    )
 
 def register_atlet(request):
     return render(request, "register_atlet.html")
@@ -19,29 +12,46 @@ def dashboard_atlet(request):
 
 
 def tes_kualifikasi(request):
-
-    # # DB Connection
-    # con = connectDb()
-    # cur = con.cursor()
-        
-    # # SQL Query
-    # cur.execute(""" SELECT tanggal FROM ujian_kualifikasi; """)   
-    # tanggal_pelaksanaan = cur.fetchall()     
-    # cur.execute(""" SELECT tempat FROM ujian_kualifikasi; """)   
-    # tempat_pelaksanaan = cur.fetchall()   
-
-    # content = {
-    #     'tanggal_pelaksanaan' : tanggal_pelaksanaan,
-    #     'tempat_pelaksanaan' : tempat_pelaksanaan,
-    # }
-
-
     return render(request, "tes_kualifikasi.html")
 
 
 def form_kualifikasi(request):
 
-    return render(request, "form_kualifikasi.html")
+    # DB Connection
+    cur = connection.cursor()
+
+    # SQL Query
+   
+    # cur.execute(""" SELECT * FROM ujian_kualifikasi; """)   
+    # dataUjian = cur.fetchall() 
+
+    # sql_batch, sql_tahun, sql_tempat, sql_tanggal = ([] for i in range(4))
+    # for data in dataUjian:
+    #         sql_batch.append(data[0])
+    #         sql_tahun.append(data[1])
+    #         sql_tempat.append(data[2])
+    #         sql_tanggal.append(str(data[3]))
+    #         print(sql_tanggal)
+
+    cur.execute(""" SELECT tanggal FROM ujian_kualifikasi; """)   
+    tanggal = cur.fetchall() 
+    cur.execute(""" SELECT tempat FROM ujian_kualifikasi; """)   
+    tempat = cur.fetchall() 
+
+    sql_tanggal = []
+    for data in tanggal:
+            sql_tanggal.append(str(data[0]))
+    sql_tempat = []
+    for data in tempat:
+            sql_tempat.append(data[0])
+
+  
+    context =  {
+        "tempat" : sql_tempat,
+        "tanggal" :  sql_tanggal,
+    }
+
+    return render(request, "form_kualifikasi.html",  context)
 
 
 def daftar_event(request):
@@ -62,3 +72,36 @@ def enrolled_event(request):
 
 def daftar_sponsor(request):
     return render(request, "daftar_sponsor.html")
+
+def datetime_to_string(data):
+    listTanggal = str(data[0]).split("-")
+    result = ""
+    result += listTanggal[1]
+    result += " "
+    if listTanggal[2] == "01" :
+        result += "Januari"
+    elif listTanggal[2] == "02" :
+        result += "Februari"
+    elif listTanggal[2] == "03" :
+        result += "Maret"
+    elif listTanggal[2] == "04" :
+        result += "April"
+    elif listTanggal[2] == "05" :
+        result += "Mei"
+    elif listTanggal[2] == "06" :
+        result += "Juni"
+    elif listTanggal[2] == "07" :
+        result += "Juli"
+    elif listTanggal[2] == "08" :
+        result += "Agustus"
+    elif listTanggal[2] == "09" :
+        result += "September"
+    elif listTanggal[2] == "10" :
+        result += "Oktober"
+    elif listTanggal[2] == "11" :
+        result += "November"
+    elif listTanggal[2] == "12" :
+        result += "Desember"
+    result += " "
+    result += listTanggal[0]  
+    return result
