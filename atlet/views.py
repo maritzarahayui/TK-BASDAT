96dc47b705507a0ad9ec9fd77d22631ff62896fb
Emily Rumia Naomi <emilyrumiaa@gmail.com>
2023-05-22 14:23:19 +0700

    Set Up Database

--- atlet/views.py
+++ atlet/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+import psycopg2
 
+def connectDb():
+    return psycopg2.connect(
+        user='postgres',
+        password='VqhDHXgGPL8avdUHqCWP',
+        host='containers-us-west-72.railway.app',
+        databases='railway',
+        port='7729',
+    )
 
 def register_atlet(request):
     return render(request, "register_atlet.html")
@@ -10,10 +19,28 @@ def dashboard_atlet(request):
 
 
 def tes_kualifikasi(request):
+
+    # # DB Connection
+    # con = connectDb()
+    # cur = con.cursor()
+        
+    # # SQL Query
+    # cur.execute(""" SELECT tanggal FROM ujian_kualifikasi; """)   
+    # tanggal_pelaksanaan = cur.fetchall()     
+    # cur.execute(""" SELECT tempat FROM ujian_kualifikasi; """)   
+    # tempat_pelaksanaan = cur.fetchall()   
+
+    # content = {
+    #     'tanggal_pelaksanaan' : tanggal_pelaksanaan,
+    #     'tempat_pelaksanaan' : tempat_pelaksanaan,
+    # }
+
+
     return render(request, "tes_kualifikasi.html")
 
 
 def form_kualifikasi(request):
+
     return render(request, "form_kualifikasi.html")
 
 
